fed696924e364ca083712647275ce3649d639449
Flavya <flavyamarya007@gmail.com>
2023-07-28 13:08:37 -0300

    Liçãojs

---
+++ index.js
@@ -0,0 +1,6 @@
+var receberNome = prompt("Digite seu nome")
+var quantasLetras = receberNome.length
+
+document.write ("<p>" + receberNome + "</p>")
+
+document.write ("<p>" + quantasLetras + "</p>")
